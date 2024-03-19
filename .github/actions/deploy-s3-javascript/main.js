1afad0256766d391c0c232a0e92f86a0be2f5a03
eduardo.latorrevandeperre <eduardo.latorrevandeperre@wbdcontractor.com>
2024-03-19 16:58:52 -0300

    Added s3 connection

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -8,12 +8,28 @@ node_module within the action SHOULD NOT BE IGNORED
 github action will not install these dependencies
 */
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run(){
     // Use messages from GithubAction
     core.notice('Hello from my custom JavaScript Action!');
+
+    // 1-Get input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true }); // Can be true even if action is false
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2-Upload files
+    // AWS CLI is pre install in ubuntu-latest
+    // ` This is JS notation 
+    const s3Uri = `s3://${bucket}`; // Passing vars into s3 string in JS
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`); // Run a command from JS
+
+    // To send request to REST GITHUB API to get access to GitHub context objects
+    // github.getOctokit
 }
 
 run();
+
+// gha-custom-action-demo
