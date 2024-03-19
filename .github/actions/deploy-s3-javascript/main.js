2138726314b54103245b76d4c88da079b7152c3b
eduardo.latorrevandeperre <eduardo.latorrevandeperre@wbdcontractor.com>
2024-03-19 11:16:48 -0300

    Fixed require tag

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -7,13 +7,13 @@ npm install @actions/core @actions/github @actions/exec
 node_module within the action SHOULD NOT BE IGNORED
 github action will not install these dependencies
 */
-const core = required('@actions/core');
-const github = required('@actions/github');
-const exec = required('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run(){
-    // Use messages from GithubActio
-    core.notice('Hello from my custom JavaScript Action!')
+    // Use messages from GithubAction
+    core.notice('Hello from my custom JavaScript Action!');
 }
 
 run();
